cfb530f1830633a17836708808a3c2062683a819
Rakesh Kumar Sharma <rakesh@avsstech.com>
2024-06-20 21:37:44 +0530

    string.xml, color.xml, harcodecode removal, var,val

---
+++ app/src/main/java/com/example/androidlearning/kotlinConcept/Test.kt
@@ -0,0 +1,11 @@
+package com.example.androidlearning.kotlinConcept
+
+fun main(args: Array<String>) {
+         //val , var
+//    var a = 100
+//    a = 10000
+
+    val a = 100
+//    a = 10000
+    print(a)
+}
