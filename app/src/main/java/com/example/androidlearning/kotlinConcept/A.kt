94936408efc86d9bd67ed1aaf86c84bd22d63d71
Rakesh Kumar Sharma <rakesh@avsstech.com>
2024-06-21 21:24:25 +0530

    string interpolation, loop, when, function, class instance, conditional statemenet

---
+++ app/src/main/java/com/example/androidlearning/kotlinConcept/A.kt
@@ -0,0 +1,6 @@
+package com.example.androidlearning.kotlinConcept
+
+class A {
+    var name: String = "Kotlin"
+    var age: Int = 30
+}
--- app/src/main/java/com/example/androidlearning/kotlinConcept/Test.kt
+++ app/src/main/java/com/example/androidlearning/kotlinConcept/Test.kt
@@ -5,7 +5,83 @@ fun main(args: Array<String>) {
 //    var a = 100
 //    a = 10000
 
-    val a = 100
-//    a = 10000
-    print(a)
+//    val a = 100
+////    a = 10000
+//    print(a)
+
+//    var name: String
+//    name = "Kotlin"
+//    println(name)
+
+//    add(100, 12)
+//    val sum = add(100, 12)
+//    println(sum)
+
+
+//    val a = A()
+////    println(a.name)
+////    println(a.age)
+//    println("Your name is ${a.name} and your age is ${a.age}")
+//    println("Your name is "+a.name+" and age is "+a.age)
+
+//    val a = 20000
+//    val b = 3000
+//
+//    if (a > b){
+//        println("$a is bigger")
+//    }else{
+//        println("$b is bigger")
+//    }
+
+//    for(i in 1..100){
+//        println(i)
+//    }
+
+
+    printDay(50)
+
+
+}
+
+
+fun add(a: Int,b: Int): Unit{
+//    var a = 100
+//    var b = 200
+//    val sum = a + b
+//    println(sum)
+
+    val sum = a + b
+//    println(sum)
+//    return sum
+
+}
+
+
+fun printDay(num: Int){
+    when(num){
+        1 -> {
+            print("Monday")
+        }
+        2 ->{
+            println("Tuesday")
+        }
+        3 ->{
+            println("Wednesday")
+        }
+        4 ->{
+            println("Thursday")
+        }
+        5 -> {
+            println("Friday")
+        }
+        6 -> {
+            println("Saturday")
+        }
+        7 -> {
+            println("Sunday")
+        }
+        else ->{
+            println("Invalid input")
+        }
+    }
 }
