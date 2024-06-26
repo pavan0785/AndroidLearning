04cfb966615c87e487488919a16772e4989ff926
pavan0785 <pavan0785@gmail.com>
2024-06-26 21:14:14 +0530

    android layout extension

--- app/src/main/java/com/example/androidlearning/MainActivity.kt
+++ app/src/main/java/com/example/androidlearning/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.androidlearning
 
 import android.os.Bundle
+import android.telephony.TelephonyCallback.UserMobileDataStateListener
 import android.util.Log
 import android.view.View
 import android.widget.Button
@@ -11,18 +12,37 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity: AppCompatActivity(), View.OnClickListener {
     lateinit var btnClick: Button
-    lateinit var tvDisplay: TextView
     var initValue: Int = 0
-    lateinit var etInput: EditText
+    lateinit var etFname: EditText
+    lateinit var etMname: EditText
+    lateinit var etLname: EditText
+    lateinit var etEmailid: EditText
+    lateinit var etMobile: EditText
+    lateinit var tvDisplay1: TextView
+    lateinit var tvDisplay2: TextView
+    lateinit var tvDisplay3: TextView
+    lateinit var tvDisplay4: TextView
+    lateinit var tvDisplay5: TextView
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btnClick = findViewById(R.id.btn_click)
-        tvDisplay = findViewById(R.id.tv_display)
-        etInput = findViewById(R.id.et_input)
+        etFname = findViewById(R.id.et_input1)
+        etMname = findViewById(R.id.et_input2)
+        etLname = findViewById(R.id.et_input3)
+        etEmailid = findViewById(R.id.et_input4)
+        etMobile = findViewById(R.id.et_input5)
+        tvDisplay1 = findViewById(R.id.tv_display1)
+        tvDisplay2 = findViewById(R.id.tv_display2)
+        tvDisplay3 = findViewById(R.id.tv_display3)
+        tvDisplay4 = findViewById(R.id.tv_display4)
+        tvDisplay5 = findViewById(R.id.tv_display5)
 
-//        tvDisplay.text = initValue.toString()
+
+        //   tvDisplay.text = initValue.toString()
 
         btnClick.setOnClickListener(this)
     }
@@ -34,6 +54,9 @@ class MainActivity: AppCompatActivity(), View.OnClickListener {
 //        initValue++
 //        tvDisplay.text = "You have clicked me"
 //        tvDisplay.text = initValue.toString()
-        tvDisplay.text = etInput.text.toString()
-    }
+          tvDisplay1.text = etFname.text.toString()
+          tvDisplay2.text = etMname.text.toString()
+          tvDisplay3.text = etLname.text.toString()
+          tvDisplay4.text = etEmailid.text.toString()
+          tvDisplay5.text = etMobile.text.toString()    }
 }
