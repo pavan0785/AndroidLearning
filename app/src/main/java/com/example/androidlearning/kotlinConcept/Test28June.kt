a5185c28efee0c74cf21d6af6107586fa0bc99e5
Rakesh <rakesh@avsstech.com>
2024-07-03 21:46:38 +0530

    data class/ Object/ Any classes

--- app/src/main/java/com/example/androidlearning/kotlinConcept/Test28June.kt
+++ app/src/main/java/com/example/androidlearning/kotlinConcept/Test28June.kt
@@ -1,12 +1,25 @@
 package com.example.androidlearning.kotlinConcept
 
+import com.example.androidlearning.model.UserInfo
+
 fun main() {
-    var name: String? = "Rakesh"
-//    name = "Kotlin"
+//    var name: String? = "Rakesh"
+////    name = "Kotlin"
 //    name = null
 
 
 
 
-    println(name!!.length)
+//    println(name!!.length)
+    val data = UserInfo("Rakesh", "Sharma");
+    val dataCopy = data.copy("A", "B")
+    println(data.hashCode())
+    println(dataCopy.hashCode())
+    println(data.toString())
+    println(dataCopy.toString())
+    println(data.equals(dataCopy))
+    println(data.component1())
+    println(data.component2())
+
+
 }
---
+++ app/src/main/java/com/example/androidlearning/model/UserInfo.kt
@@ -0,0 +1,6 @@
+package com.example.androidlearning.model
+
+data class UserInfo(
+    val userFname: String,
+    val userLName: String
+)
