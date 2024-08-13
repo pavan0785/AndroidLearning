7c6559471683641fa688c4bfe48e32486b9d45f6
Rakesh <rakesh@avsstech.com>
2024-08-13 23:21:11 +0530

    Logging interceptor

--- app/src/main/java/com/example/androidlearning/retrofit/RetrofitClient.kt
+++ app/src/main/java/com/example/androidlearning/retrofit/RetrofitClient.kt
@@ -2,13 +2,26 @@ package com.example.androidlearning.retrofit
 
 import com.example.androidlearning.interfaces.ApiInterface
 import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
     private val retrofitClient: Retrofit.Builder by lazy {
+        val LOGGING = HttpLoggingInterceptor()
+        LOGGING.setLevel(HttpLoggingInterceptor.Level.BODY)
+
+        val okHttpClient = OkHttpClient.Builder()
+        okHttpClient.addInterceptor(LOGGING)
+        okHttpClient.connectTimeout(20, TimeUnit.SECONDS)
+        okHttpClient.readTimeout(20, TimeUnit.SECONDS)
+        okHttpClient.writeTimeout(20, TimeUnit.SECONDS)
+
         Retrofit.Builder()
             .baseUrl("https://fakestoreapi.com")
+            .client(okHttpClient.build())
             .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
     }
 
