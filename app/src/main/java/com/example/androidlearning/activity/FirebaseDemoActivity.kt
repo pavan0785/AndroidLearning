92c0caf8d9fb01d91b11609d4bc29cacfdb9696e
Rakesh <rakesh@avsstech.com>
2024-08-21 22:53:51 +0530

    firebase Db correction

--- app/src/main/java/com/example/androidlearning/activity/FirebaseDemoActivity.kt
+++ app/src/main/java/com/example/androidlearning/activity/FirebaseDemoActivity.kt
@@ -15,33 +15,46 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class FirebaseDemoActivity : AppCompatActivity(), View.OnClickListener {
     lateinit var binding: ActivityFirebaseDemoBinding
-    lateinit var firebaseDb: FirebaseDatabase
+//    lateinit var firebaseDb: FirebaseDatabase
     lateinit var databaseRef: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityFirebaseDemoBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        firebaseDb = FirebaseDatabase.getInstance()
-        databaseRef = firebaseDb.getReference("EmployeeInfo")
+//        firebaseDb = FirebaseDatabase.getInstance()
+//        databaseRef = firebaseDb.getReference("EmployeeInfo")
+        databaseRef = Firebase.database.reference
 
         binding.btnSaveUser.setOnClickListener(this)
     }
 
     override fun onClick(view: View?) {
-        databaseRef.addValueEventListener(object : ValueEventListener{
-            override fun onDataChange(snapshot: DataSnapshot) {
-                databaseRef.setValue(EmployeeInfo(binding.etFname.text.toString(), binding.etLname.text.toString(), binding.etEmail.text.toString()))
+//        databaseRef.addValueEventListener(object : ValueEventListener{
+//            override fun onDataChange(snapshot: DataSnapshot) {
+//                databaseRef.child("employee").push().setValue(EmployeeInfo(binding.etFname.text.toString(), binding.etLname.text.toString(), binding.etEmail.text.toString()))
+//                Toast.makeText(this@FirebaseDemoActivity, "Data successfully saved.", Toast.LENGTH_SHORT).show()
+//            }
+//
+//            override fun onCancelled(error: DatabaseError) {
+//                Toast.makeText(this@FirebaseDemoActivity, "Something error", Toast.LENGTH_SHORT).show()
+//            }
+//
+//        })
+
+        databaseRef.child("employee_1")
+            .push()
+            .setValue(EmployeeInfo(binding.etFname.text.toString(), binding.etLname.text.toString(), binding.etEmail.text.toString()))
+            .addOnSuccessListener {
                 Toast.makeText(this@FirebaseDemoActivity, "Data successfully saved.", Toast.LENGTH_SHORT).show()
             }
-
-            override fun onCancelled(error: DatabaseError) {
+            .addOnFailureListener {
                 Toast.makeText(this@FirebaseDemoActivity, "Something error", Toast.LENGTH_SHORT).show()
             }
-
-        })
     }
 }
